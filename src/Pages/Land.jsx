3aa6cafc08232eb1af108bc9b2878c9575f0b27c
Gisele-123 <giselemigisha53@gmail.com>
2024-08-17 09:53:52 -0700

    i don't know wht this sisishfufjker i jsttt

--- src/Pages/Land.jsx
+++ src/Pages/Land.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../Assets/logo.png"
 
 const Land=()=>{
     return(
-        <div style={{display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center", gap:"20px", height:"100vh"}}>
-            <div style={{fontSize:"40px"}}>KamiConnekt</div>
+        <div style={{display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center", height:"100vh"}}>
+            <div>
+                <img src={logo} alt="" />
+            </div>
             <Link to="/register" style={{border:"none", backgroundColor:"#0474BA", padding:"10px 20px", textDecoration:"none", color:"#fff", fontWeight:"bold", borderRadius:"5px"}}>Continue</Link>
         </div>
     )
