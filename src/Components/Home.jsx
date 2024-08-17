ae04da25c28999ea33033b830854bd888d36aaaf
Gisele-123 <giselemigisha53@gmail.com>
2024-08-17 10:46:41 -0700

    sidirsdhkhfs

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,127 +1,44 @@
 import React, { useState } from "react";
-import ur from "../Assets/ur.png";
-import alu from "../Assets/alu.png";
-import cmu from "../Assets/cmu.png";
-import students from "../Assets/grad.png";
+import ur from "../Assets/ur.png"
+import alu from "../Assets/alu.png"
+import cmu from "../Assets/cmu.png"
+import students from "../Assets/grad.png"
 import ChatBot from "../Components/ChatBot";
 import { Link } from "react-router-dom";
 
 const Home = () => {
-  const [showChatBot, setShowChatBot] = useState(false);
+    const [showChatBot, setShowChatBot] = useState(false);
 
-  const handleChatWithUsClick = () => {
-    setShowChatBot(true);
-  };
+    const handleChatWithUsClick = () => {
+        setShowChatBot(true);
+    };
 
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          padding: "40px",
-          gap: "40px",
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <p style={{ color: "#252525", fontWeight: "bold", fontSize: "60px" }}>
-            Only government approved universities
-          </p>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              gap: "20px",
-            }}
-          >
-            <div>
-              <img src={ur} alt="" width={80} />
-            </div>
-            <div>
-              <img src={alu} alt="" width={80} />
-            </div>
-            <div>
-              <img src={cmu} alt="" width={80} />
-            </div>
-          </div>
-          <p
-            style={{ color: "#0474BA", fontWeight: "normal", fontSize: "20px" }}
-          >
-            'Now secure your future with online'
-          </p>
-          <div style={{ display: "flex", gap: "40px" }}>
-            <button
-              style={{
-                padding: "10px 20px",
-                border: "none",
-                backgroundColor: "#0474BA",
-                borderRadius: "5px",
-                color: "#fff",
-                fontWeight: "bold",
-                transition: "transform 0.3s, box-shadow 0.3s",
-              }}
-              onMouseEnter={(e) => {
-                e.target.style.transform = "scale(1.1) translateY(10px)";
-                e.target.style.boxShadow = "0 10px 20px rgba(0, 0, 0, 0.2)";
-              }}
-              onMouseLeave={(e) => {
-                e.target.style.transform = "scale(1) translateY(0)";
-                e.target.style.boxShadow = "none";
-              }}
-            >
-              Create my learning roadmap
-            </button>
+    return (
 
-            <button
-              onClick={handleChatWithUsClick}
-              style={{
-                padding: "10px 20px",
-                border: "none",
-                borderColor: "#0474BA",
-                borderStyle: "solid",
-                borderWidth: "1px",
-                borderRadius: "5px",
-                color: "#0474BA",
-                fontWeight: "bold",
-              }}
-            >
-              Chat with us
-            </button>
-          </div>
+        <div style={{display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center"}}>
+            <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", padding: "40px", gap: "40px" }}>
+                <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+                    <p style={{ color: "#252525", fontWeight: "bold", fontSize: "60px" }}>Only government approved universities</p>
+                    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", gap: "20px" }}>
+                        <div><img src={ur} alt="" width={80} /></div>
+                        <div><img src={alu} alt="" width={80} /></div>
+                        <div><img src={cmu} alt="" width={80} /></div>
+                    </div>
+                    <p style={{ color: "#0474BA", fontWeight: "normal", fontSize: "20px" }}>'Now secure your future with online'</p>
+                    <div style={{ display: "flex", gap: "40px" }}>
+                        <Link to="/meet" style={{ padding: "10px 20px", border: "none", textDecoration:"none", backgroundColor: "#0474BA", borderRadius: "5px", color: "#fff", fontWeight: "bold" }}>Meet college mentors</Link>
+                        <button onClick={handleChatWithUsClick} style={{ padding: "10px 20px", border: "none", borderColor: "#0474BA", borderStyle: "solid", borderWidth: "1px", borderRadius: "5px", color: "#0474BA", fontWeight: "bold" }}>Chat with us</button>
+                    </div>
+                </div>
+                <div>
+                    <img src={students} alt="" width={480} />
+                </div>
+            </div>
+            <div style={{width:"40%", display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center"}}>
+                {showChatBot && <ChatBot />}
+            </div>
         </div>
-        <div>
-          <img src={students} alt="" width={480} />
-        </div>
-      </div>
-      <div
-        style={{
-          width: "40%",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        {showChatBot && <ChatBot />}
-      </div>
-    </div>
-  );
-};
+    )
+}
 
 export default Home;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,117 +1,23 @@
 import React from "react";
 import { Icon } from "@iconify/react/dist/iconify.js";
-import "../Styles/navbar.css";
+import "../Styles/navbar.css"
 
-const Navbar = () => {
-  return (
-    <div
-      className="navbar-container"
-      style={{
-        background: "#fff",
-        display: "flex",
-        padding: "0 40px",
-        justifyContent: "space-between",
-        alignItems: "center",
-      }}
-    >
-      <div style={{ color: "#0474BA", display: "flex", alignItems: "center" }}>
-        <img
-          src="logo.png"
-          alt="Logo"
-          style={{ width: "100px", height: "auto", marginRight: "10px" }}
-        />
-        <p
-          className="app-name"
-          style={{ fontWeight: "bold", fontSize: "25px" }}
-        >
-          KamiConnekt
-        </p>
-      </div>
+const Navbar = () =>{
+    return(
+        <div className="navbar-container" style={{ background:"#fff",display:"flex", padding: "0 40px", justifyContent:"space-between", alignItems:"center"}}>
+            <div style={{color:"#0474BA"}}>
+                <p className="app-name" style={{fontWeight:"bold", fontSize:"25px"}}>KamiConnekt</p>
+            </div>
+            <div className="" style={{display:"flex", justifyContent:"center", alignItems:"center", gap:"20px"}}>
+                <div style={{display:"flex", justifyContent:"center", alignItems:"center", gap:"10px", fontSize:"15px"}}><p>Online</p><Icon icon="ep:arrow-down-bold" color="0474BA"/></div>
+                <div style={{display:"flex", justifyContent:"center", alignItems:"center", gap:"10px", fontSize:"15px"}}><p>Post-graduation</p><Icon icon="ep:arrow-down-bold" color="0474BA"/></div>
+                <div style={{display:"flex", justifyContent:"center", alignItems:"center", gap:"10px", fontSize:"15px"}}><p>Undergraduate</p><Icon icon="ep:arrow-down-bold" color="0474BA"/></div>
+                <div style={{display:"flex", justifyContent:"center", alignItems:"center", gap:"10px", fontSize:"15px"}}><p>Upskilling Certifications</p><Icon icon="ep:arrow-down-bold" color="0474BA"/></div>
+                <div style={{display:"flex", justifyContent:"center", alignItems:"center", gap:"10px", fontSize:"15px"}}><p>Study abroad</p><Icon icon="ep:arrow-down-bold" color="0474BA"/></div>
+                <div style={{display:"flex", justifyContent:"center", alignItems:"center", gap:"10px", fontSize:"15px"}}><p>Advanced diploma</p><Icon icon="ep:arrow-down-bold" color="0474BA"/></div>
+            </div>
+        </div>
+    )
+}
 
-      <div
-        className=""
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          gap: "20px",
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            gap: "10px",
-            fontSize: "15px",
-          }}
-        >
-          <p>Online</p>
-          <Icon icon="ep:arrow-down-bold" color="0474BA" />
-        </div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            gap: "10px",
-            fontSize: "15px",
-          }}
-        >
-          <p>Post-graduation</p>
-          <Icon icon="ep:arrow-down-bold" color="0474BA" />
-        </div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            gap: "10px",
-            fontSize: "15px",
-          }}
-        >
-          <p>Undergraduate</p>
-          <Icon icon="ep:arrow-down-bold" color="0474BA" />
-        </div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            gap: "10px",
-            fontSize: "15px",
-          }}
-        >
-          <p>Upskilling Certifications</p>
-          <Icon icon="ep:arrow-down-bold" color="0474BA" />
-        </div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            gap: "10px",
-            fontSize: "15px",
-          }}
-        >
-          <p>Study abroad</p>
-          <Icon icon="ep:arrow-down-bold" color="0474BA" />
-        </div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            gap: "10px",
-            fontSize: "15px",
-          }}
-        >
-          <p>Advanced diploma</p>
-          <Icon icon="ep:arrow-down-bold" color="0474BA" />
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default Navbar;
+export default Navbar
